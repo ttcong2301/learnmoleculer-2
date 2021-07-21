3692c8c54029ce940f6346d3faef9f72033b6456
Anks <anks999999@gmail.com>
2021-07-21 15:26:24 +0700

    init code

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true,
+		commonjs: true,
+		es6: true,
+		jquery: false,
+		jest: true,
+		jasmine: true,
+	},
+	extends: 'airbnb-base',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: '2018',
+	},
+	rules: {
+		indent: [
+			'warn',
+			'tab',
+			{ SwitchCase: 1 },
+		],
+		quotes: [
+			'warn',
+			'single',
+		],
+		semi: [
+			'error',
+			'always',
+		],
+		'no-var': [
+			'error',
+		],
+		'no-console': [
+			'off',
+		],
+		'no-tabs': [
+			'off',
+		],
+		'no-unused-vars': [
+			'warn',
+		],
+		'no-mixed-spaces-and-tabs': [
+			'warn',
+		],
+		'func-names': 'off',
+		'global-require': 'off',
+	},
+};
